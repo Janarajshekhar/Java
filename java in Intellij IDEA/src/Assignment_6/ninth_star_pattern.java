8738f4ef573cf038a7e6e3c8594b5fc28c5f631d
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-04-27 10:43:16 +0530

    Refactor ninth_star_pattern class to include main method for star pattern display

--- java in Intellij IDEA/src/Assignment_6/ninth_star_pattern.java
+++ java in Intellij IDEA/src/Assignment_6/ninth_star_pattern.java
@@ -1,5 +1,20 @@
 package Assignment_6;
 
 public class ninth_star_pattern {
-    
+    public static void main(String []args){
+        int i,j;
+        char k=64;
+        for(i=1;i<=5;i++) {
+            k=(char)(64+i);
+            for (j = 1; j <= 5; j++) {
+                if (j >= i) {
+                    System.out.print(" " + k + " ");
+                    k++;
+                }
+                else
+                    System.out.print("   ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
