2a2b83e0f52990dcbbca0bf57b96bd00a91c8c04
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-05-10 20:37:36 +0530

    Implement area and circumference calculations for circles using multilevel inheritance and interfaces

--- College Assignment 2nd Sem/src/multilevel_inheritance_using_interface.java
+++ College Assignment 2nd Sem/src/multilevel_inheritance_using_interface.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
-public class multilevel_inharitance_using_interface {
+import java.util.Scanner;
+interface circle{
+    public void area(double r);
+}
+class area_of_circle implements circle{
+    public void area(double r){
+        double a=3.14*r*r;
+        System.out.println("Area = "+a);
+    }
+}
+class circumference_of_circle extends area_of_circle{
+    public void circumference(Double r){
+        double c=2*3.14*r;
+        System.out.println("Circumference = "+c);
+    }
+}
+public class multilevel_inheritance_using_interface {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius of circle : ");
+        double r=sc.nextDouble();
+        circumference_of_circle c=new circumference_of_circle();
+        c.area(r);
+        c.circumference(r);
+    }
 }
