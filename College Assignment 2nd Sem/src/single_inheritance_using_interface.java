cb12e78d586c9dbb291ef604693064b1aeb2f657
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-05-10 09:53:05 +0530

    Implement area calculation using interface for length and width input

--- College Assignment 2nd Sem/src/single_inheritance_using_interface.java
+++ College Assignment 2nd Sem/src/single_inheritance_using_interface.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+interface Box{
+    void length_width(double length,double width);
+}
+class area implements Box{
+    public void length_width(double length,double width){
+        double area=length*width;
+        System.out.println("Area = "+area);
+    }
+}
 public class single_inheritance_using_interface {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter length and width : ");
+        double l=sc.nextDouble();
+        double w=sc.nextDouble();
+        area a=new area();
+        a.length_width(l,w);
+    }
 }
