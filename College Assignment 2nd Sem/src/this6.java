eab7b304c5df43134c023641eb7cb8ade29e48c1
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-06-13 23:28:27 +0530

    Refactor Circle_ and CircumferenceCalculator classes; add circumference calculation and output display

--- College Assignment 2nd Sem/src/this6.java
+++ College Assignment 2nd Sem/src/this6.java
@@ -1,12 +1,21 @@
+class Circle_ {
+    double radius;
 
-class Data {
-    Data(this6 obj) {
-        System.out.println("Constructor received object: " + obj);
+    Circle_(double radius) {
+        this.radius = radius;
+        new CircumferenceCalculator(this); // Passing this to another class
+    }
+}
+
+class CircumferenceCalculator {
+    CircumferenceCalculator(Circle_ c) {
+        double circumference = 2 * 3.14 * c.radius;
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
 
 public class this6 {
     public static void main(String[] args) {
-
+        new Circle_(5);
     }
 }
