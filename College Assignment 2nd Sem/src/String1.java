b0cd2361db705639014291d3fb115bb9f968297e
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-06-15 11:21:03 +0530

    Refactor String classes: add package declarations and empty class definitions for String2, String3, String4, and String_Palindrome; remove final4 class

---
+++ College Assignment 2nd Sem/src/String1.java
@@ -0,0 +1,11 @@
+
+public class final4 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = " World";
+        System.out.println("First String: " + str1);
+        System.out.println("Second String: " + str2);
+        String str3 = str1.concat(str2);
+        System.out.println("After concat : " + str3);
+    }
+}
